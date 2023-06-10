b52bd8afdeb4ca4a63228492748ba331133891dd
renatadsyf <renatadsyf22@gmail.com>
2023-06-10 15:37:05 +0700

    sidang

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","tokokita");
+
+if(!$conn){
+	echo "gagal konek database";
+} else {
+	
+};
+
+?>
